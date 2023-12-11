523806baa91dd0a45b9a7eb7b5f8cccef39750d6
Cttran3 <99286656+Cttran3@users.noreply.github.com>
2023-12-11 11:52:13 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 8
+//
+//  Created by Camille Tran on 11/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
